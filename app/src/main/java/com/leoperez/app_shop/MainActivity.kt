7ec25f3b78fae4af64d71b710f06d61c0495c51d
Leo Perez Hinojosa <leoperezhinojosa.dev@gmail.com>
2024-02-25 12:35:33 +0100

    7º Commit: Crear articulo nuevo funcionando

--- app/src/main/java/com/leoperez/app_shop/MainActivity.kt
+++ app/src/main/java/com/leoperez/app_shop/MainActivity.kt
@@ -14,12 +14,31 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private var articleList: MutableList<Article> = ArticleProvider.articles
     private lateinit var adapter: ArticleAdapter
+    private val manager = LinearLayoutManager(this)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.buttonAddArticle.setOnClickListener {
+            addArticle()
+        }
         initRecyclerView()
+    }
+
+    private fun addArticle() {
+        val article = Article(
+            "000F",
+            "Nuevo Artículo",
+            "Nueva Marca",
+            "https://cdn-icons-png.flaticon.com/512/5540/5540433.png",
+            0.0,
+            0
+        )
+        articleList.add(index = 0,article)
+        adapter.notifyItemInserted(0)
+        manager.scrollToPositionWithOffset(0, 10)
+        Toast.makeText(this, "Nuevo artículo (predeterminado) creado", Toast.LENGTH_SHORT).show()
     }
 
     private fun initRecyclerView() {
@@ -27,7 +46,6 @@ class MainActivity : AppCompatActivity() {
             articleList,
             onClickListener = { article -> onItemSelected(article) },
             onClickDelete = { position -> onItemDeleted(position) } )
-        val manager = LinearLayoutManager(this)
         binding.mainRecyclerView.layoutManager = manager
         binding.mainRecyclerView.adapter = adapter
 
@@ -36,9 +54,10 @@ class MainActivity : AppCompatActivity() {
     private fun onItemDeleted(position: Int) {
         articleList.removeAt(position)
         adapter.notifyItemRemoved(position)
+        Toast.makeText(this, "Artículo BORRADO", Toast.LENGTH_SHORT).show()
     }
     private fun onItemSelected(article: Article) {
         Toast.makeText(this, article.name, Toast.LENGTH_SHORT).show()
-
+        // Implementar la navegación a la pantalla de detalle
     }
 }
