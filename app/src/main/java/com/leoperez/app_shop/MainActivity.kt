6ea73d433dac9ff6d13a0679533fcaa383aec894
Leo Perez Hinojosa <leoperezhinojosa.dev@gmail.com>
2024-02-25 01:45:23 +0100

    3º Commit: MainActivity y clases adapter reseteadas

--- app/src/main/java/com/leoperez/app_shop/MainActivity.kt
+++ app/src/main/java/com/leoperez/app_shop/MainActivity.kt
@@ -8,33 +8,8 @@ import com.leoperez.app_shop.databinding.ActivityMainBinding
 import com.leoperez.app_shop.objects_models.Articles.articles
 
 class MainActivity : AppCompatActivity() {
-
-    lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    binding = ActivityMainBinding.inflate(layoutInflater)
-    setContentView(binding.root)
-    initRecyclerView()
-}
-
-    private fun initRecyclerView() {
-        val recyclerView = binding.listaArticulos
-        recyclerView.adapter = ArticleAdapter(
-            articles,
-            {
-                // Eliminar artículo
-                articles.removeAt(it)
-                recyclerView.adapter?.notifyItemRemoved(it)
-            },
-            {
-                // Actualizar artículo
-                recyclerView.adapter?.notifyItemChanged(it)
-            },
-            {
-                // Crear artículo
-                recyclerView.adapter?.notifyItemInserted(articles.size - 1)
-            }
-        )
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
     }
 }
